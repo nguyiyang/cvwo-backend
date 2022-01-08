a07af2178aa243ab95dd6ebb529fead5641f83fd
nguyiyang <nguyiyang47@gmail.com>
2022-01-08 16:45:04 +0800

    Implement basic CRUD operations

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,25 @@ class TasksController < ApplicationController
       tasks = Task.all
       render json: tasks
     end
+
+    def create
+      task = Task.create(task_params)
+      render json: task
+    end
+
+    def update
+      task = Task.find(params[:id])
+      task.update(task_params)
+      render json: task
+    end
+
+    def destroy
+      Task.destroy(params[:id])
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:title)
+    end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "tasks#index"
-  resources :tasks, only: [:index]
+  resources :tasks, only: [:index, :create, :update, :destroy]
 end
